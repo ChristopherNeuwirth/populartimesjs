139cecdffa86cc6b322328ede63415d751dd65bb
Christopher Neuwirth <christopher.neuwirth@mhp.com>
2020-06-11 17:02:29 +0200

    chore: prepare for publishing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { resolve } from 'path';
 import { config } from 'dotenv';
 config({ path: resolve(__dirname, '../.env') });
-
 import { Populartimes } from './populartimes';
 
-// just for development purpose. If finished make this primary entry point for popular times.
+// just for development purpose
 
 const populartimes = new Populartimes(process.env.GOOGLEAPIKEY);
 const testPlace = 'ChIJc9Ra71_RmUcRzdxRjVQjqXo'; // IKEA Ludwigsburg
